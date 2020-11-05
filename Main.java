1b4131463679f795cb690b773178fbff11a5799c
Abdulah Omran <abdulah.omran@patelhs.org>
2020-11-05 14:21:53 +0000

    Created Arrays and A for loop

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
     int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
     String[] names = new String[15];
     for (int i = 0; i < 14; i++) {
-      
+       
     }
   }
 }
